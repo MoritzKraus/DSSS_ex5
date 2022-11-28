a355361d78798b54701eebf26270405192d6b727
MoritzKraus <moritz.kraus@gmx.net>
2022-11-28 10:48:20 +0100

    implemented multi_colour snowflakes

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,5 +1,6 @@
 import turtle
 import numpy as np
+import random
 
 
 def main(speed=0, bg_color="grey"):
@@ -11,22 +12,23 @@ def main(speed=0, bg_color="grey"):
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
+    turtle.colormode(255)
+    #myTurtle.pencolor("red")
 
 
     for _ in range(10):
         # define some params
         size = 18
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
-
-        """TODO: set snowflake color here (one of the colors defined above)"""
+        rand1 = random.randrange(0,254)
+        rand2 = random.randrange(0,254)
+        rand3 = random.randrange(0,254)
 
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
         myTurtle.pendown()
+        myTurtle.color(rand1, rand2, rand3)
 
         # draw the snowflake
         for _ in range(8):
